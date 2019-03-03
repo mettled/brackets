3ee5c36fc3fbd263fb7ed179d5cec24fe953193e
Sergey Rom <smettled@gmail.com>
2019-03-03 22:36:14 +0300

    feat: add new answer task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ var tempstr = str;
+  var lngStr = 0;
+  var i = 0;
+    for (i=0; ;i++){
+
+        if (i<bracketsConfig.length){
+          var conf = (bracketsConfig[i]);
+          conf = conf.join(',').replace(',','');
+
+        // alert((conf));
+          var tempstr = tempstr.replace(conf,'');
+        // alert(tempstr);
+        }
+
+        if (tempstr.length == 0 ){
+          return true;
+        } else if (i == 0 && lngStr == tempstr.length) {
+          return false;
+        }
+
+        if (i == 0){
+          lngStr = tempstr.length;
+        } else if (i == bracketsConfig.length){
+          i = -1;
+        }
+
+
+    }
+
 }
